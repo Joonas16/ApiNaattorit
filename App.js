323d654ddb06e69e3495d282d118e2658b1fce23
Joonas Aaltonen <joonas.aaltonen16@gmail.com>
2021-02-02 13:24:02 +0200

    BottomTabs & Views

--- App.js
+++ App.js
@@ -1,21 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { faHome, faList, faCloudSun } from '@fortawesome/free-solid-svg-icons'
 
-export default function App() {
+import Koti from './views/Koti';
+import Hakemistot from './views/Hakemistot';
+import Saa from './views/Saa';
+
+const Tab = createBottomTabNavigator();
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({focused}) => {
+            if (route.name === 'Koti') {
+              return <FontAwesomeIcon size={18} icon={ faHome } color={focused ? '#11cdde' : '#C3C3C3'} />;
+            } else if (route.name === 'Hakemistot') {
+              return <FontAwesomeIcon size={18} icon={ faList } color={focused ? '#11cdde' : '#C3C3C3'} />;
+            } else {
+             return <FontAwesomeIcon size={18} icon={ faCloudSun } color={focused ? '#11cdde' : '#C3C3C3'} />;
+            }
+          },
+        }
+      )}
+      
+      tabBarOptions={{activeTintColor: '#11cdde', inactiveTintColor: '#C3C3C3', labelStyle: {fontSize: 14}}}>
+        <Tab.Screen name="Koti" component={Koti} />
+        <Tab.Screen name="Hakemistot" component={Hakemistot} />
+        <Tab.Screen name="Saa" component={Saa} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
